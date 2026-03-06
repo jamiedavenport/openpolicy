13b4522aafc085e704103797733319b3c5417afd
Jamie Davenport <x@jxd.dev>
2026-03-06 19:47:28 +0000

    adds tanstack example (#5)

---
+++ examples/tanstack/vite.config.ts
@@ -0,0 +1,22 @@
+import { openPolicy } from "@openpolicy/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+	server: {
+		port: 3000,
+	},
+	plugins: [
+		tsConfigPaths(),
+		openPolicy({
+			configs: ["policy.config.ts", "terms.config.ts"],
+			formats: ["html"],
+			outDir: "src/policies",
+		}),
+		tanstackStart(),
+		// react's vite plugin must come after start's vite plugin
+		viteReact(),
+	],
+});
