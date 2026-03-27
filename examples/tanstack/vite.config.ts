0a0aa62e79acd4e686caadb0796a17c2a11755a4
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2026-03-27 04:56:27 +0000

    configure nitro

--- examples/tanstack/vite.config.ts
+++ examples/tanstack/vite.config.ts
@@ -2,6 +2,7 @@ import { openPolicy } from "@openpolicy/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
@@ -19,6 +20,7 @@ export default defineConfig({
 			outDir: "src/policies",
 		}),
 		tanstackStart(),
+		nitro(),
 		// react's vite plugin must come after start's vite plugin
 		viteReact(),
 		process.env.ANALYZE === "true" &&
